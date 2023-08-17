bd51d10780c3aea0c940719926e569f0ee35bccb
RamYadavilli21 <ram.yadavilli21042000@gmail.com>
2023-08-18 00:00:36 +0530

    Changes

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assests/logo.png";
+import SearchIcon from "../assests/Search icon.jpg";
+import navbar from "./Navbar.module.css";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <div className={navbar.navBar}>
+      <img className={navbar.img} src={logo} alt="logo" />
+      <div className={navbar.searchContainer}>
+        <input
+          className={navbar.searchInput}
+          placeholder="Search a album of your choice"
+        />
+        <button className={navbar.searchBtn}>
+          <img src={SearchIcon} alt="searchIcon" />
+        </button>
+      </div>
+      <Button className={navbar.button}>Give Feedback</Button>
+    </div>
+  );
+};
+
+export default Navbar;
