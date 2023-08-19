b77efbfde90de689cbd9e1b5baac236cf6bbe12a
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2023-08-19 17:59:30 +0530

    Hero Section Implemented

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+import HeroImg from "../assests/vibrating-headphone.png";
+
+const Hero = () => {
+  return (
+    <div className="HeroContainer">
+      <div className="heroText">
+        <h1>100 Thousand Songs ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <img className="heroImg" src={HeroImg} alt="HeroImage" />
+    </div>
+  );
+};
+
+export default Hero;
